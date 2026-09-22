d71673930e14bb2342434d6014e964e9445c271f
Sonia Corbin <soniacorbin4@gmail.com>
2026-09-22 09:14:11 -0400

    feat(owasp): screenshot prevention FLAG_SECURE (debug exempt)

--- android/app/src/main/kotlin/com/storyblock/ca/MainActivity.kt
+++ android/app/src/main/kotlin/com/storyblock/ca/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.storyblock.ca
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!isDebugMode()) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE
+            )
+        }
+    }
+
+    private fun isDebugMode(): Boolean {
+        return (applicationInfo.flags and android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE) != 0
+    }
+}
